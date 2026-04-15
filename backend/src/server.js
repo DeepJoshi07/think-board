92744559cb5f7ae2bff5df4bc181d7e1c061f663
Deep Joshi <joshideep972@gmail.com>
2026-04-15 11:15:22 +0530

    created mongoose schema of notes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import notesRouter from "./routes/notes.route.js"
+import {connectDB} from "./config/db.js"
+import "dotenv/config"
 
-const PORT = 5001;
+const PORT = process.env.PORT;
 
 const app = express();
+
+connectDB()
 
 app.use("/api/notes",notesRouter)
 

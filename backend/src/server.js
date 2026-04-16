9e9ee623b82e480bd1c2bf54173b16ae9228b2bc
Deep Joshi <joshideep972@gmail.com>
2026-04-16 11:30:53 +0530

    removing env

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,20 @@
 import express from "express";
-import notesRouter from "./routes/notes.route.js"
-import {connectDB} from "./config/db.js"
-import "dotenv/config"
+import notesRouter from "./routes/notes.route.js";
+import { connectDB } from "./config/db.js";
+import "dotenv/config";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 const PORT = process.env.PORT;
 
 const app = express();
 
-connectDB()
+app.use(express.json());
+app.use(rateLimiter);
 
-app.use(express.json())
-app.use(rateLimiter)
+app.use("/api/notes", notesRouter);
 
-app.use("/api/notes",notesRouter)
-
-app.listen(PORT,() => {
-    console.log(`server is running on port ${PORT}`)
-})
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+  });
+});

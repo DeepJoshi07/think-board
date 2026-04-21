23da7d09a6e1fc0fb1a5e151c7f905199e385bd3
Deep Joshi <joshideep972@gmail.com>
2026-04-21 09:53:38 +0530

    making the app production ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,37 @@ import notesRouter from "./routes/notes.route.js";
 import { connectDB } from "./config/db.js";
 import "dotenv/config";
 import rateLimiter from "./middleware/rateLimiter.js";
-import cors from "cors"
-const PORT = process.env.PORT;
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
-app.use(cors("http://localhost:5173"))
+
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true
+  }));
+}
+
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
